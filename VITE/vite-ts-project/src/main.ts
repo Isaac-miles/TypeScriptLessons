6be5359a346a24467ab206cdac5edb1619207e35
isaac-miles <isaacnice247@gmail.com>
2023-10-09 12:05:56 +0100

    Vanila Ts project with vite

--- VITE/vite-ts-project/src/main.ts
+++ VITE/vite-ts-project/src/main.ts
@@ -1,6 +1,4 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+import './css/style.css'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
