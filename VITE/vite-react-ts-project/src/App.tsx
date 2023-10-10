5310cf59af98da99b0787335b076be9d761d8259
isaac-miles <isaacnice247@gmail.com>
2023-10-10 15:56:19 +0100

    useReducer hooks

--- VITE/vite-react-ts-project/src/App.tsx
+++ VITE/vite-react-ts-project/src/App.tsx
@@ -1,24 +1,11 @@
-import React,{useState,useEffect, useCallback} from 'react'
+import Counter from "./CounterHooks"
 
-interface User{
-    id:number,
-    username:string
-}
 function App() {
-    const [count, setCount] = useState(0)
-    const [users, setUsers] = useState<User[] | null>()
-
-    useEffect(()=>{
-    console.log('mounting')
-    console.log('users', users)
-    return ()=>console.log('unmounting')
-    }, [users])
 
   return (
-    <div className='App'>
-        <h1>{count}</h1>
-        <button onClick={}> Add </button>
-    </div>
+    <>
+      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+    </>
   )
 }
 
