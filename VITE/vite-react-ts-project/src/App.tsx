44502dd70e4cf09d4dca7a30d781cee81368d4ba
isaac-miles <isaacnice247@gmail.com>
2023-10-11 10:46:45 +0100

    updated for mini project

--- VITE/vite-react-ts-project/src/App.tsx
+++ VITE/vite-react-ts-project/src/App.tsx
@@ -1,13 +1,11 @@
-import Counter from "./CounterHooks"
-import { CounterProvider ,initState} from "./contex/counterContext"
 
 function App() {
 
-  return (
-    <CounterProvider  count={initState.count} userName={initState.userName}>
-      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
-    </CounterProvider>
-  )
-}
-
-export default App
+    return (
+      <div className="App">
+  
+      </div>
+    )
+  }
+  
+  export default App
