b5288e08691e957fb1844ab4d6d0e96990d8b32a
isaac-miles <isaacnice247@gmail.com>
2023-10-16 20:37:02 +0100

    added the prepare call back

--- React-Redux/react-redux/src/App.js
+++ React-Redux/react-redux/src/App.js
@@ -1,9 +1,11 @@
-
+import PostsList from './features/posts/postsList';
+import AddPostForm from './features/posts/AddPostForm';
 
 function App() {
   return (
     <main className="App">
-   
+   <PostsList />
+   <AddPostForm/>
     </main>
   );
 }
