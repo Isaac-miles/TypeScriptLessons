a3f8e7884ba8e85cb26407b6226e2d71ff7ceec3
isaac-miles <isaacnice247@gmail.com>
2023-10-18 10:32:29 +0100

    updated fetching of post

--- React-Redux/react-redux/src/features/posts/postsList.js
+++ React-Redux/react-redux/src/features/posts/postsList.js
@@ -3,18 +3,19 @@ import {useSelector,useDispatch} from 'react-redux'
 import { selectAllPosts,getPostsError,getPostsStatus,fetchPosts } from './postsSlice'
 import PostsExcerpt from './PostsExcerpt'
 const PostsList = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
     const posts = useSelector(selectAllPosts)
     const postStatus = useSelector(getPostsStatus)
     const error = useSelector(getPostsError)
 
 
-    useEffect(()=>{
-        if(postStatus === 'idle'){
-            dispatch(fetchPosts())
-        }
-    },[postStatus,dispatch])
+    // useEffect(()=>{
+    //     if(postStatus === 'idle'){
+    //         dispatch(fetchPosts())
+    //     }
+    // },[postStatus,dispatch])
+    //fetching of posts is now being dispatched at index
     let content;
     if (postStatus === 'loading') {
         content = <p>"Loading..."</p>;
--- React-Redux/react-redux/src/index.js
+++ React-Redux/react-redux/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { store } from './app/store';
 import {Provider} from 'react-redux'
 import { fetchUsers } from './features/users/usersSlice';
-
+import { fetchPosts } from './features/posts/postsSlice';
 store.dispatch(fetchUsers())
-
+store.dispatch(fetchPosts())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
