9f3296727f1fd99de1d97aaeae8806f0396c52f4
isaac-miles <isaacnice247@gmail.com>
2023-10-19 13:31:47 +0100

    update

--- React-Redux/react-redux/src/features/posts/postsList.js
+++ React-Redux/react-redux/src/features/posts/postsList.js
@@ -1,19 +1,48 @@
-import { useSelector } from "react-redux";
-import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
+// import { useSelector } from "react-redux";
+// import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
+// import PostsExcerpt from "./PostsExcerpt";
+
+// const PostsList = () => {
+
+//     const orderedPostIds = useSelector(selectPostIds)
+//     const postStatus = useSelector(getPostsStatus);
+//     const error = useSelector(getPostsError);
+
+//     let content;
+//     if (postStatus === 'loading') {
+//         content = <p>"Loading..."</p>;
+//     } else if (postStatus === 'succeeded') {
+//         content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+//     } else if (postStatus === 'failed') {
+//         content = <p>{error}</p>;
+//     }
+
+//     return (
+//         <section>
+//             {content}
+//         </section>
+//     )
+// }
+// export default PostsList
+
 import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from './postsSlice';
 
 const PostsList = () => {
-
-    const orderedPostIds = useSelector(selectPostIds)
-    const postStatus = useSelector(getPostsStatus);
-    const error = useSelector(getPostsError);
+    const {
+        data: posts,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetPostsQuery('getPosts')
 
     let content;
-    if (postStatus === 'loading') {
+    if (isLoading) {
         content = <p>"Loading..."</p>;
-    } else if (postStatus === 'succeeded') {
-        content = orderedPostIds.map(postId => <PostsExcerpt key={postId} postId={postId} />)
-    } else if (postStatus === 'failed') {
+    } else if (isSuccess) {
+        content = posts.ids.map(postId => <PostsExcerpt key={postId} postId={postId} />)
+    } else if (isError) {
         content = <p>{error}</p>;
     }
 
--- React-Redux/react-redux/src/index.js
+++ React-Redux/react-redux/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
-import {Provider} from 'react-redux'
-import { fetchUsers } from './features/users/usersSlice';
-import { fetchPosts } from './features/posts/postsSlice';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-store.dispatch(fetchUsers())
-store.dispatch(fetchPosts())
+
+
+// store.dispatch(fetchUsers())
+// store.dispatch(fetchPosts())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
