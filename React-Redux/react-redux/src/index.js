0a7f7d74c5fc82f053ff7ea3c35d4b170fd6fc61
isaac-miles <isaacnice247@gmail.com>
2023-10-21 19:33:48 +0100

    removed store.dispatch from index

--- React-Redux/react-redux/src/index.js
+++ React-Redux/react-redux/src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
-store.dispatch(fetchUsers())
-store.dispatch(fetchPosts())
+// store.dispatch(fetchUsers())
+// store.dispatch(fetchPosts())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
