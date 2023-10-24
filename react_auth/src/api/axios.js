1d37cd2dab3a869dc427991f3e442250f82153ac
isaac-miles <isaacnice247@gmail.com>
2023-10-24 19:05:28 +0100

    created axios private

--- react_auth/src/api/axios.js
+++ react_auth/src/api/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL ='http://192.168.8.101:5005'
 export default axios.create({
     // baseURL: 'http://EBKTECH-L930:5005'
-    baseURL: 'http://192.168.8.101:5005'
+    baseURL: BASE_URL
     // baseURL: 'http://localhost:3500'
+});
+
+export const  axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers:{'Content-Type': 'application/json'},
+    withCredentials:true
 });
---
+++ react_auth/src/hooks/useAxiosPrivate.js
@@ -0,0 +1,19 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+import useRefreshToken from './useRefreshToken'
+import useAuth from "./useAuth";
+
+const useAxiosPrivate = () =>{
+    const refresh = useRefreshToken();
+    const {auth} = useAuth();
+
+    useEffect(()=>{
+        
+
+
+    },[auth,refresh])
+
+    return axiosPrivate
+}
+
+export default useAxiosPrivate
