d8e195c44a11b00046f10543ff896274b148cf78
isaac-miles <isaacnice247@gmail.com>
2023-10-25 16:18:00 +0100

    changing the base url to point to the local server

--- react_auth/src/api/axios.js
+++ react_auth/src/api/axios.js
@@ -4,7 +4,6 @@ const BASE_URL = 'http://localhost:3500'
 
 export default axios.create({
     baseURL: BASE_URL
-    // baseURL: 'http://localhost:3500'
 });
 
 export const  axiosPrivate = axios.create({
