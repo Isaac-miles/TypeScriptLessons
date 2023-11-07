97478a88a6a637a022653fe717a47ab2ba379bc7
isaac-miles <isaacnice247@gmail.com>
2023-11-07 22:29:59 +0100

    .

--- nextflix/app/page.tsx
+++ nextflix/app/page.tsx
@@ -1,15 +1,17 @@
+import Banner from '@/components/Banner'
 import Header from '@/components/Header'
 
 
 export default function Home() {
   return (
     <div className='relative h-screen bg-gradient-to-b from-green-900/10  to-[#010511] lg:h-[140vh]'>
+
       <Header />
 
 
 
     <main className="">
-      
+      <Banner/>
     </main>
     </div>
   )
