d5b3db8a65cac6f4012c6173b051eb204106bd54
isaac-miles <isaacnice247@gmail.com>
2023-11-07 13:37:24 +0100

    added heroIcon

--- nextflix/components/Header.tsx
+++ nextflix/components/Header.tsx
@@ -5,14 +5,24 @@ const Header = () => {
     const imgUrl = new URL(`/public/img/netflix-logo.png`, import.meta.url).href
   return (
     <header>
-      <div>
+      <div className="flex items-center space-x-2 md:space-x-10">
         <Image 
         src={imgUrl}
         alt="netflix"
         width={150}
         height={150}
         className="cursor-pointer object-contain"/>
+
+        <ul className="hidden space-x-4 md:flex">
+            <li className="headerLink">Home</li>
+            <li className="headerLink">Tv Shows</li>
+            <li className="headerLink">Movie</li>
+            <li className="headerLink">New & Popular</li>
+            <li className="headerLink">My List</li>
+        </ul>
       </div>
+
+      <div></div>
     </header>
   )
 }
