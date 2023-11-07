0c5b1fd1a99832b6968b9b2ac47c236a2fba02b5
isaac-miles <isaacnice247@gmail.com>
2023-11-07 14:11:33 +0100

    designed the header

--- nextflix/components/Header.tsx
+++ nextflix/components/Header.tsx
@@ -1,8 +1,32 @@
+'use client'
+
 import Image from "next/image"
+import Link from "next/link"
+
+import { BellIcon, MagnifyingGlassIcon as SearchIcon, } from "@heroicons/react/20/solid"
+import { useEffect, useState } from "react"
 
 
 const Header = () => {
+    const [isScrolled, setIscrolled] = useState(false)
+
     const imgUrl = new URL(`/public/img/netflix-logo.png`, import.meta.url).href
+    const accImgUrl = new URL(`/public/img/acc-img.png`, import.meta.url).href
+
+    useEffect(()=>{
+        const handleScroll = ()=>{
+            if(window.scrollY > 0){
+                setIscrolled(true)
+            }else{
+                setIscrolled(false)
+            }
+        }
+       
+        window.addEventListener('scroll', handleScroll)
+
+        return window.removeEventListener('scroll',handleScroll)
+    },[])
+
   return (
     <header>
       <div className="flex items-center space-x-2 md:space-x-10">
@@ -22,7 +46,19 @@ const Header = () => {
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline "/>
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="hidden h-6 w-6 sm:inline "/>
+        <Link href={'/account'}>
+          <Image 
+        src={accImgUrl}
+        alt="account"
+        width={30}
+        height={30}
+        className="cursor-pointer rounded"/>
+        </Link>
+      </div>
     </header>
   )
 }
