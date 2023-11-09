c34b1f43f5acad6adbb0cc94ca8cc5b89e87735e
isaac-miles <isaacnice247@gmail.com>
2023-11-09 22:20:33 +0100

    added and modified the left and right arrows

--- nextflix/components/Row.tsx
+++ nextflix/components/Row.tsx
@@ -3,7 +3,7 @@
 import { Movies } from "@/types"
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid"
 import Thumbnail from "./Thumbnail"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 
 type RowProps = {
@@ -14,16 +14,27 @@ type RowProps = {
 }
 
 const Row = ({title,movies} : RowProps) => {
-  
+    const [isMoved, setIsMoved] = useState(false)
     const rowRef = useRef<HTMLDivElement>(null)
 
+    const handleClick = (direction:string)=>{
+        setIsMoved(true)
+        if(rowRef.current){
+            const {scrollLeft, clientWidth}= rowRef.current
+            
+            const scrollTo = direction === 'left'? scrollLeft-clientWidth
+            : scrollLeft + clientWidth
+
+            rowRef.current.scrollTo({left:scrollTo, behavior:"smooth"})
+        }
+    }
 
   return (
     <div className="h-40 space-y-0.5 md:space-y-2  ">
         <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
 
     <div className="group relative md:-ml-2" >
-            <ChevronLeftIcon  className="rowIcons"/>
+            <ChevronLeftIcon  className={`rowIcons left-2 ${!isMoved && 'hidden' }`}onClick={()=>handleClick("left")}/>
 
             <div ref={rowRef} className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5  md:p-2">
                     {movies.map((movie)=>(
@@ -33,7 +44,7 @@ const Row = ({title,movies} : RowProps) => {
                   
                 </div>
      
-            <ChevronRightIcon  className="rowIcons"/>
+            <ChevronRightIcon  className={`rowIcons right-2`}  onClick={()=>handleClick("right")}/>
 
         </div> 
     </div>
