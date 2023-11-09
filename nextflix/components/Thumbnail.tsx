a0b6c2bb15d1b2116f6243344a3fa8c8173961a7
isaac-miles <isaacnice247@gmail.com>
2023-11-09 20:16:30 +0100

    added custom scrollbars

--- nextflix/components/Thumbnail.tsx
+++ nextflix/components/Thumbnail.tsx
@@ -10,7 +10,7 @@ const Thumbnail = ({movie}:ThumbnailProps) => {
     const thumbImage = new URL(`${BASE_URL_THUMBNAIL}${movie?.backdrop_path || movie?.poster_path}`, import.meta.url).href
 
   return (
-    <div className="relative h-28 min-w-[180px]">
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
         <Image 
 
          src={thumbImage} 
