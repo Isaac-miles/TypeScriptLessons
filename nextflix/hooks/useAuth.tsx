dc516c43d0e8bc3b824eee89277e148fa95f33c1
isaac-miles <isaacnice247@gmail.com>
2023-11-12 18:15:42 +0100

    creating useAuth

--- nextflix/hooks/useAuth.tsx
+++ nextflix/hooks/useAuth.tsx
@@ -1,6 +1,16 @@
-
+import { useState, useEffect, useContext, createContext,useMemo } from "react"
+import { useRouter } from "next/router"
+import { auth } from "../firebase"
+import {
+     createUserWithEmailAndPassword,
+     onAuthStateChanged,
+     signInWithEmailAndPassword,
+     signOut,
+     User
+     } from "@firebase/auth"
 
 function useAuth() {
+
   return (
     <div>useAuth</div>
   )
