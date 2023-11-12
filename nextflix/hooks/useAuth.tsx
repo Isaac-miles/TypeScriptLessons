75b9fe2a1e363bcb7809bea4b75411b180e84e39
isaac-miles <isaacnice247@gmail.com>
2023-11-12 18:31:46 +0100

    created the signUp function

--- nextflix/hooks/useAuth.tsx
+++ nextflix/hooks/useAuth.tsx
@@ -10,10 +10,26 @@ import {
      } from "@firebase/auth"
 
 function useAuth() {
+    const [loading, setLoading] = useState(false)
+    const [user, setUser] = useState<User | null>()
+    const router = useRouter()
 
-  return (
-    <div>useAuth</div>
-  )
+   const signUp =async (email:string, password:string) => {
+        setLoading(true)
+        await createUserWithEmailAndPassword(auth,email, password)
+        .then((userCredentials):void =>{
+            setUser(userCredentials.user)
+            router.push('/')
+            setLoading(false)
+        }).catch((err)=>{
+            if (err instanceof Error){
+                return err.message
+            }
+        }).finally(()=>setLoading(false))
+   }
+
+
+  return[]
 }
 
 export default useAuth
