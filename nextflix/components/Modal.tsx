ba645e90dd1568564acf07f573d9d94f7ed19f46
isaac-miles <isaacnice247@gmail.com>
2023-11-14 13:54:28 +0100

    updated modal btn

--- nextflix/components/Modal.tsx
+++ nextflix/components/Modal.tsx
@@ -16,9 +16,13 @@ function Modal() {
   return (
     <MuiModal open={modal} onClose={handleClose}>
     <>
-      <button onClick={handleClose}>
+      <button onClick={handleClose} className='modalbtn absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#962c2c] hover:bg-[#181818'>
         <XMarkIcon className='h-6 w-6'/>
       </button>
+
+      <div>
+        
+      </div>
     </>
     </MuiModal>
 
