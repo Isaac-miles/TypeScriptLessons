1ae90ce930adefe8af401b1a8a8c27548095991d
isaac-miles <isaacnice247@gmail.com>
2023-11-16 16:12:40 +0100

    added list of options

--- nextflix/components/Plans.tsx
+++ nextflix/components/Plans.tsx
@@ -2,9 +2,13 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
+import useAuth from '@/hooks/useAuth'
+import { CheckIcon } from '@heroicons/react/24/outline'
 
 function Plans() {
-    // const imgUrl = new URL(`/public/img/netflix-logo.png`, import.meta.url).href
+    const imgUrl = new URL(`/public/img/netflix-logo.png`, import.meta.url).href
+    const {logOut} = useAuth()
+
 
   return (
     <div>
@@ -12,19 +16,38 @@ function Plans() {
             <title>Netnaija flix</title>
         </Head>
 
-        <header>
-        <Link href='/'>
-        <Image 
-        src='/netflix-logo.png'
-        alt="netflix"
-        width={100}
-        height={100}
-        priority
-        className="cursor-pointer object-contain w-auto h-auto"/>
-        </Link>
-        <button className='text-lg font-medium hover:underline'>Sign Out</button>
+        <header className='border-b border-white/10 bg-[#141414] '>
+            <Link href='/' as ={'image'}>
+            <Image 
+            src={imgUrl}
+            alt="netflix"
+            width={100}
+            height={100}
+            priority
+            className="cursor-pointer object-contain w-auto h-auto"/>
+            </Link>
+            <button className='text-lg font-medium hover:underline' onClick={logOut}>Sign Out</button>
         </header>
         
+        <main className='pt-28 '>
+            <h1 className='mb-3 text-3xl font-medium'>Choose a plan</h1>
+            <ul>
+                <li className='flex items-center gap-x-2 text-lg'>
+                    <CheckIcon className='h-7 w-7 text-[#E50914]'/>
+                    Watch all you want. Ad-free.
+                </li>
+                <li className='flex items-center gap-x-2 text-lg'>
+                    <CheckIcon className='h-7 w-7 text-[#E50914]'/>
+                    Recommendations just for you
+                </li>
+                <li className='flex items-center gap-x-2 text-lg'>
+                    <CheckIcon className='h-7 w-7 text-[#E50914]'/>
+                    change or cancel your plan anytime
+                </li>
+             
+            </ul>
+            
+        </main>
     </div>
   )
 }
