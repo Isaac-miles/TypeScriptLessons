d5540608b348a59b1bf2d9913731ca289eed1215
isaac-miles <isaacnice247@gmail.com>
2023-11-27 22:23:20 +0100

    added transpile config but not working

--- nextflix/next.config.js
+++ nextflix/next.config.js
@@ -21,3 +21,9 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withTM = require('next-transpile-modules')(['@stripe/firestore-stripe-payments']); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
+  reactStrictMode:true,
+});
