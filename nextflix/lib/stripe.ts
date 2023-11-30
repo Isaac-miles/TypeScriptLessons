2eeeff85a7e32f0663402f6dbddf636b0699f2d1
isaac-miles <isaacnice247@gmail.com>
2023-11-30 23:50:56 +0100

    back to trying other methods

--- nextflix/lib/stripe.ts
+++ nextflix/lib/stripe.ts
@@ -1,39 +1,20 @@
-import {getApp} from "@firebase/app"
-import {getStripePayments,getProducts} from "@invertase/firestore-stripe-payments"
 
+import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
+import app from "./firebase";
 
-const app = getApp();
-export const payments = getStripePayments(app,{
-    productsCollection:"products",
-    customersCollection:"customers"
-})
+ const payments = getStripePayments(app, {
+    productsCollection: "products",
+    customersCollection: "customers",
+  });
 
- const products = await getProducts(payments,{
-    includePrices:true,
-    activeOnly:true
-})
+ export const loadCheckout =async (priceId:string) => {
+    await createCheckoutSession(payments, {
+        price:priceId,
+        success_url:window.location.origin,
+        cancel_url:window.location.origin
+    })
+    .then((snapshot)=>window.location.assign(snapshot.url))
+    .catch((err)=>console.log(err.message))
+ }
 
-
-export default products
-
-
-
-// import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
-// import app from "./firebase";
-
-//  const payments = getStripePayments(app, {
-//     productsCollection: "products",
-//     customersCollection: "customers",
-//   });
-
-//  export const loadCheckout =async (priceId:string) => {
-//     await createCheckoutSession(payments, {
-//         price:priceId,
-//         success_url:window.location.origin,
-//         cancel_url:window.location.origin
-//     })
-//     .then((snapshot)=>window.location.assign(snapshot.url))
-//     .catch((err)=>console.log(err.message))
-//  }
-
-//  export default payments
+ export default payments
