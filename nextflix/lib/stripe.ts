6571b07939d2c8797688766450359fa4538ed235
isaac-miles <isaacnice247@gmail.com>
2023-11-30 23:07:01 +0100

    added @invertase/firestore

--- nextflix/lib/stripe.ts
+++ nextflix/lib/stripe.ts
@@ -1,20 +1,29 @@
-import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
-import app
- from "./firebase";
+import {getApp} from "@firebase/app"
+import {getStripePayments} from "@invertase/firestore-stripe-payments"
 
- const payments = getStripePayments(app, {
-    productsCollection: "products",
-    customersCollection: "customers",
-  });
 
- export const loadCheckout =async (priceId:string) => {
-    await createCheckoutSession(payments, {
-        price:priceId,
-        success_url:window.location.origin,
-        cancel_url:window.location.origin
-    })
-    .then((snapshot)=>window.location.assign(snapshot.url))
-    .catch((err)=>console.log(err.message))
- }
 
- export default payments
+
+
+
+
+
+// import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
+// import app from "./firebase";
+
+//  const payments = getStripePayments(app, {
+//     productsCollection: "products",
+//     customersCollection: "customers",
+//   });
+
+//  export const loadCheckout =async (priceId:string) => {
+//     await createCheckoutSession(payments, {
+//         price:priceId,
+//         success_url:window.location.origin,
+//         cancel_url:window.location.origin
+//     })
+//     .then((snapshot)=>window.location.assign(snapshot.url))
+//     .catch((err)=>console.log(err.message))
+//  }
+
+//  export default payments
