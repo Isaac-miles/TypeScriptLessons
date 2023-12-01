b5d8c7c6362cc431899e272c76dc0f505dfbf1ea
isaac-miles <isaacnice247@gmail.com>
2023-12-01 21:52:22 +0100

    debugging

--- nextflix/app/app.tsx
+++ nextflix/app/app.tsx
@@ -2,34 +2,23 @@ import getMoviesData from "@/utils/getData"
 
 import Home from "./home"
 import getProduct from "./getProduct"
+import  products  from "@/lib/stripe"
 
 
+async function getData() {
+  const products = await getProduct()
 
-// import  products  from "@/lib/stripe"
-// async function getData() {
-  // const res = await fetch('https://api.example.com/...')
-  // const products = await getProduct()
-
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  if (!products) {
+    throw new Error('Failed to fetch data')
+  }
  
-  // if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    // throw new Error('Failed to fetch data')
-  // }
+  return products
+}
  
-  // return res.json()
-// }
- 
-// export default async function Page() {
-//   const data = await getData()
- 
-//   return <main></main>
-// }
 
-
-async function App() {
-
+export default async function App() {
+  const data = await getData()
+  console.log('produc', data)
   const {
     netflixOriginals,
     trendingNow,
@@ -43,19 +32,20 @@ async function App() {
   } = await getMoviesData()
 
   return (
-    <Home 
-    netflixOriginals={netflixOriginals}
-    trendingNow={trendingNow}
-    topRated={topRated}
-    actionMovies={actionMovies}
-    comedyMovies={comedyMovies}
-    horrorMovies={horrorMovies}
-    romanceMovies={romanceMovies}
-    documentaries={documentaries} 
-    // products={products}
-    />
-  
+    // <Home 
+    // netflixOriginals={netflixOriginals}
+    // trendingNow={trendingNow}
+    // topRated={topRated}
+    // actionMovies={actionMovies}
+    // comedyMovies={comedyMovies}
+    // horrorMovies={horrorMovies}
+    // romanceMovies={romanceMovies}
+    // documentaries={documentaries} 
+    // // products={products}
+    // />
+  <div>
+    
+  </div>
   )
 }
 
-export default App
