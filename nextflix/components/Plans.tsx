b1525038d33dadc71e6ff056e47a20c9f1ac99d4
isaac-miles <isaacnice247@gmail.com>
2023-12-04 11:39:03 +0100

    added button

--- nextflix/components/Plans.tsx
+++ nextflix/components/Plans.tsx
@@ -30,7 +30,7 @@ function Plans({products}:{products:ProductType}) {
             <button className='text-lg font-medium hover:underline' onClick={logOut}>Sign Out</button>
         </header>
         
-        <main className='pt-28 max-w-5xl  px-5 pb-12  transition-all md:px-10'>
+        <main className='mx-auto pt-28 max-w-5xl  px-5 pb-12  transition-all md:px-10'>
             <h1 className='mb-3 text-3xl font-medium'>Choose a plan</h1>
             <ul>
                 <li className='flex items-center gap-x-2 text-lg'>
@@ -58,7 +58,19 @@ function Plans({products}:{products:ProductType}) {
                 {/* <Table /> */}
                 <Table products={products} selectedPlan ={selectedPlan}/>
 
-                <button>Subscribe</button>
+                <button
+            disabled={!selectedPlan || isBillingLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl shadow hover:bg-[#f6121d] md:w-[420px] ${
+              isBillingLoading && 'opacity-60'
+            }`}
+            onClick={subscribeToPlan}
+          >
+            {isBillingLoading ? (
+              <Loader color="dark:fill-gray-300" />
+            ) : (
+              'Subscribe'
+            )}
+          </button>
             </div>
         </main>
     </div>
