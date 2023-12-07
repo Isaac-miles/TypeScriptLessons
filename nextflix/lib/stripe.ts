7b387a267ed81d038c445d7e3dfd6dad0164e87e
isaac-miles <isaacnice247@gmail.com>
2023-12-07 18:54:47 +0100

    .

--- nextflix/lib/stripe.ts
+++ nextflix/lib/stripe.ts
@@ -1,20 +1,20 @@
 
-import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
-import app from "./firebase";
+// import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
+// import app from "./firebase";
 
- const payments = getStripePayments(app, {
-    productsCollection: "products",
-    customersCollection: "customers",
-  });
+//  const payments = getStripePayments(app, {
+//     productsCollection: "products",
+//     customersCollection: "customers",
+//   });
 
- export const loadCheckout = async (priceId:string) => {
-    await createCheckoutSession(payments, {
-        price:priceId,
-        success_url:window.location.origin,
-        cancel_url:window.location.origin
-    })
-    .then((snapshot)=>window.location.assign(snapshot.url))
-    .catch((err)=>console.log(err.message))
- }
+//  export const loadCheckout = async (priceId:string) => {
+//     await createCheckoutSession(payments, {
+//         price:priceId,
+//         success_url:window.location.origin,
+//         cancel_url:window.location.origin
+//     })
+//     .then((snapshot)=>window.location.assign(snapshot.url))
+//     .catch((err)=>console.log(err.message))
+//  }
 
  export default payments
