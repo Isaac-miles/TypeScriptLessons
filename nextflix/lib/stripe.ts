2ff5a707d3261b2361d622bc0eee55c72ebab873
isaac-miles <isaacnice247@gmail.com>
2023-12-08 17:19:47 +0100

    changes:checkout

--- nextflix/lib/stripe.ts
+++ nextflix/lib/stripe.ts
@@ -1,15 +1,15 @@
 
 import { createCheckoutSession,getStripePayments } from "@stripe/firestore-stripe-payments";
 import app from "./firebase";
-import { getProduct } from "@/app/stripe-util/getStripesUtils";
+
+
  const payments = getStripePayments(app, {
     productsCollection: "products",
     customersCollection: "customers",
   });
 
-  const payment = await getProduct()
  export const loadCheckout = async (priceId:string) => {
-    await createCheckoutSession(payment, {
+    await createCheckoutSession(payments, {
         price:priceId,
         success_url:window.location.origin,
         cancel_url:window.location.origin
