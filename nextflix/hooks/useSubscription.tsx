72e2fa8bd2fc4aa17d3c0e495711e82c361b9bc2
isaac-miles <isaacnice247@gmail.com>
2023-12-18 22:53:49 +0100

    query to get the user's subscription

--- nextflix/hooks/useSubscription.tsx
+++ nextflix/hooks/useSubscription.tsx
@@ -4,6 +4,13 @@ import {useState,useEffect} from 'react'
 export default function useSubscription(user:User | null) {
     const [subscription, setSubscription] = useState<null>(null)
 
+    useEffect(()=>{
+        if(!user) return
+
+        on
+    },[])
+
+
   return (
     <div>useSubscription</div>
   )
