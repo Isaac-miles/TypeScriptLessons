d9b8030865fc7a3f858e3b7d94aa5c2818b1640b
isaac-miles <isaacnice247@gmail.com>
2023-12-18 23:10:46 +0100

    created the useSub

--- nextflix/hooks/useSubscription.tsx
+++ nextflix/hooks/useSubscription.tsx
@@ -1,17 +1,16 @@
+import { SubscriptionType,getActiveSubscription } from '@/app/stripe-util/getStripesUtils'
 import { User } from 'firebase/auth'
 import {useState,useEffect} from 'react'
 
 export default function useSubscription(user:User | null) {
-    const [subscription, setSubscription] = useState<>(null)
+    const [subscription, setSubscription] = useState<SubscriptionType | null>(null)
 
     useEffect(()=>{
         if(!user) return
+        const activeSub =  getActiveSubscription(user.uid)
+        setSubscription(activeSub)
+    },[user])
 
-        on
-    },[])
 
-
-  return (
-    <div>useSubscription</div>
-  )
+  return setSubscription
 }
