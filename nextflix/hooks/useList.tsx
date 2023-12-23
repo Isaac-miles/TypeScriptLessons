dbefde1181ae5940325c0c91b084d2d6226abc57
isaac-miles <isaacnice247@gmail.com>
2023-12-23 21:58:44 +0100

    .

--- nextflix/hooks/useList.tsx
+++ nextflix/hooks/useList.tsx
@@ -1,7 +1,9 @@
+import { Movies } from '@/types'
 import {useState} from 'react'
+import {DocumentData} from 'firebase/firestore'
 
 function useList() {
-    
+    const [list, setList] = useState<Movies[] | DocumentData[]>([])
   return (
     <div>useList</div>
   )
