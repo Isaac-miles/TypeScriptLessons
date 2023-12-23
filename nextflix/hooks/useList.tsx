6223e02958515d088fcd90b21b6986a9b2236694
isaac-miles <isaacnice247@gmail.com>
2023-12-23 22:08:37 +0100

    .

--- nextflix/hooks/useList.tsx
+++ nextflix/hooks/useList.tsx
@@ -1,9 +1,26 @@
 import { Movies } from '@/types'
-import {useState} from 'react'
-import {DocumentData} from 'firebase/firestore'
-
-function useList() {
+import {useState, useEffect} from 'react'
+import {DocumentData,onSnapshot,collection} from 'firebase/firestore'
+import { db } from '@/lib/firebase'
+function useList(uid:string | undefined) {
     const [list, setList] = useState<Movies[] | DocumentData[]>([])
+
+    useEffect(()=>{
+        if(!uid) return
+
+        return onSnapshot(
+            collection(db, 'customers', uid, 'myList'),
+            (snapshot) => {
+              setList(
+                snapshot.docs.map((doc) => ({
+                  id: doc.id,
+                  ...doc.data(),
+                }))
+              )
+            }
+          )
+        }, [uid])
+      
   return (
     <div>useList</div>
   )
