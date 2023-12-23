9c38c8083cdb63906664e1b718179591172464e2
isaac-miles <isaacnice247@gmail.com>
2023-12-23 21:27:49 +0100

    created basicMenu

--- nextflix/app/account/page.tsx
+++ nextflix/app/account/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import {useState,useEffect} from 'react'
 import { getProduct,ProductType } from '../stripe-util/getStripesUtils'
-import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
