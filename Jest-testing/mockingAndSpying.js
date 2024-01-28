99ff7bb3fa1e7e6da4fd07c5276f23a34d1193df
isaac-miles <isaacnice247@gmail.com>
2024-01-28 17:02:16 +0100

    update

--- Jest-testing/mockingAndSpying.js
+++ Jest-testing/mockingAndSpying.js
@@ -1,2 +1,7 @@
 
 // const mockCallback = jest.fn(x => 4 + x); jest fn is a way to create a mock function
+
+test("mocking a basic function",()=>{
+    const mock = jest.fn(x=>5 + 1);
+    expect(mock(5)).toBe(10);
+})
