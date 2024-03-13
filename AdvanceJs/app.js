31057d963e1a22f976033b22b07876b95bfd7a5a
isaac-miles <isaacnice247@gmail.com>
2024-03-13 17:03:54 +0100

    this belongs to the left prop

--- AdvanceJs/app.js
+++ AdvanceJs/app.js
@@ -9,3 +9,8 @@ function whatIsThis(){
     console.log('what is this',this)
 }
 // the general rule is the value of this will always be whats on this left handside eg the above window.whatIsThis this refered to the win obj
+const obj ={
+    name:'mile',
+    age:2,
+    whatIsThis
+}
