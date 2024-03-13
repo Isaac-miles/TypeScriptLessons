cdc35605169f75dbdde95d6bc93f96fb3a3cf7a6
isaac-miles <isaacnice247@gmail.com>
2024-03-13 16:59:44 +0100

    .

--- AdvanceJs/app.js
+++ AdvanceJs/app.js
@@ -5,3 +5,7 @@ const person = {
         return `${this.name} sings Hellow world`
     }
 }
+function whatIsThis(){
+    console.log('what is this',this)
+}
+// the general rule is the value of this will always be whats on this left handside eg the above window.whatIsThis this refered to the win obj
