02df3acd3b44e7b30e8c6935aecb76b92a8937be
isaac-miles <isaacnice247@gmail.com>
2024-03-16 12:18:51 +0100

    .

--- AdvanceJs/app.js
+++ AdvanceJs/app.js
@@ -131,5 +131,5 @@ const btn = document.querySelector("#clickMe")
         }
     }
 
-    //arrow function and this
+    //arrow function and this( Arrow functions dont make their own this, they dont get a new value for the keyword this)
  
