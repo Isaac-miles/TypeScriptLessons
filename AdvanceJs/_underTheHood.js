88844c8212d3d1e9b9a78712e23207be8f25371d
isaac-miles <isaacnice247@gmail.com>
2024-03-16 21:19:07 +0100

    .

---
+++ AdvanceJs/_underTheHood.js
@@ -0,0 +1,9 @@
+/* the new keyword does four things:
+creates an empty object, sets the keyword this to be that object
+returns the object*/
+
+//below is a constructor function
+function Dog(name,breed){
+    this.name = name;
+    this.breed = breed;
+}
