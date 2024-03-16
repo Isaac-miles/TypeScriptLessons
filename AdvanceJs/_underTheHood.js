8f387042b5dd79cca80309f25d216b73e710d905
isaac-miles <isaacnice247@gmail.com>
2024-03-16 21:34:18 +0100

    .

--- AdvanceJs/_underTheHood.js
+++ AdvanceJs/_underTheHood.js
@@ -1,9 +1,24 @@
 /* the new keyword does four things:
 creates an empty object, sets the keyword this to be that object
-returns the object*/
+returns the object
+creates a link to the object's prototype*/
 
-//below is a constructor function
-function Dog(name,breed){
-    this.name = name;
-    this.breed = breed;
+class Dog{
+    constructor(name,breed){
+        this.name = name;
+        this.breed = breed;
+    }
+    bark(){
+        return `${this.name} says woof`
+    }
+    sleep(){
+        return `${this.name} is sleeping`
+    }
 }
+//below is a constructor function
+// function Dog(name,breed){
+//     console.log("This is" ,this)
+//     this.name = name;
+//     this.breed = breed;
+// }
+//prototype
