98e7b3705e0623f9f9aa5b50cf3285147c6d2f1f
isaac-miles <isaacnice247@gmail.com>
2024-03-18 11:36:30 +0100

    .

--- AdvanceJs/async.js
+++ AdvanceJs/async.js
@@ -1,2 +1,9 @@
+const {log} = console;
 //callbacks are functions used for async functionality
 //Promise in Js are Objects, they are native to the language as of ES2015, a promise can be in one of the three states, pending resolve(fulfilled) and rejected
+
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+fetch(`${BASE_URL}/1`)
+.then((res)=>log(res))
+.catch((err)=>log(err))
