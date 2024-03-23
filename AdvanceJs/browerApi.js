60739f87afa7a6bcfd4c99e59ba7892dcf7d31af
isaac-miles <isaacnice247@gmail.com>
2024-03-23 19:10:15 +0100

    .

--- AdvanceJs/browerApi.js
+++ AdvanceJs/browerApi.js
@@ -91,6 +91,11 @@ function loadMoreItems(){
 
     if(scrollDistanceToBottom < 200){
         log("loading data from an api!!");
-        
+        for(let i=0; i <10; i++){
+            const item = document.createElement("div");
+            item.classList.add("item")
+            item.textContent = "Item " + (content.children.length + 1);
+            item.style.backgroundColor = getRandomColor();
+        }
     }
 }
