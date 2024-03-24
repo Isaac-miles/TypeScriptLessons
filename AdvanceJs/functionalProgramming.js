3f2198e4f7f29fda9b09e3899ff2f67bbde20778
isaac-miles <isaacnice247@gmail.com>
2024-03-24 20:12:22 +0100

    .

--- AdvanceJs/functionalProgramming.js
+++ AdvanceJs/functionalProgramming.js
@@ -1,1 +1,3 @@
-//Functional programming is the process of building software by composing pure functions, avoiding shared state,mutable data, and side-effect
+//Functional programming is the process of building software by composing pure functions, avoiding shared state,mutable data, and side-effect, FP is often declarative rather than imperative and application state flows through pure functions.
+
+//functional programming methods (map,filter,some/every,find/findIndex,reduce)
