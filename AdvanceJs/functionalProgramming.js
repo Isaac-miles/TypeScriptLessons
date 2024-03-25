5ff5cb7211ad8608498103aa99b2cc005c775880
isaac-miles <isaacnice247@gmail.com>
2024-03-25 11:57:32 +0100

    currying

--- AdvanceJs/functionalProgramming.js
+++ AdvanceJs/functionalProgramming.js
@@ -151,3 +151,21 @@ function lowerCaseString(str){
  const slug = compose(joinWithDash,splitWords,lowerCaseString);
 
  //Currying:a curried function can be called with any number of arguments- if you call it with fewer args than it takes,it returns a "smaller" partial which you can then call with remaining arguments.
+ function addAll(a,b,c){
+    return a+ b+ c
+ }
+
+ //simplest example
+ function addCurry(a){
+    return function(b){
+        return function(c){
+            return a+b+c
+        };
+    };
+ }
+
+ function add3(x,y,z){
+    return x+y+z
+ }
+
+ 
