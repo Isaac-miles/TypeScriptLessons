49935ffa0acb5bb99b697b71e8bfeee062c6cbde
isaac-miles <isaacnice247@gmail.com>
2024-03-25 17:56:45 +0100

    .

--- AdvanceJs/fetchApi.js
+++ AdvanceJs/fetchApi.js
@@ -3,4 +3,8 @@ const POKE_URL = "https://pokeapi.co/api/v2/pokemon";
 async function getPokemon(){
     //the fetch api returns a readable streams of responses 
     const response = await fetch(POKE_URL)
+
+    //the json recieves a readable stream of responses from the fetch interface and brings it to completion
+    const data = response.json()
+    log(data)
 }
