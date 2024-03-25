46c76987fd3bcdd24b8330ce355816445045b514
isaac-miles <isaacnice247@gmail.com>
2024-03-25 12:01:45 +0100

    .

--- AdvanceJs/functionalProgramming.js
+++ AdvanceJs/functionalProgramming.js
@@ -172,6 +172,10 @@ function lowerCaseString(str){
     return function curried(...args){
         if(args.length >= fn.length){
             return fn.apply(this,args);
+        }else{
+            return function(...args2){
+                return curried.apply(this,args.concat(args2))
+            }
         }
     }
  }
