4faf691a528a0d33d7c4ebe3320df1e51a9a7336
isaac-miles <isaacnice247@gmail.com>
2024-03-27 14:44:58 +0100

    .

--- AdvanceJs/geolocationApi.js
+++ AdvanceJs/geolocationApi.js
@@ -5,14 +5,23 @@
     const navigateBtn = document.querySelector("#getLocation").addEventListener("click",()=>{
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(displayGeoData,
-            function(err){
-                log(err)
-            })
+            displayError)
         }
     });
-
+    const displayArea = document.querySelector("#locationDisplay");
     const displayGeoData = (position)=>{
         const {latitude,longitude} = position.coords;
-        const displayArea = document.querySelector("#locationDisplay");
         displayArea.textContent = `Latitude: ${latitude}, longitude: ${longitude}`
     }
+
+    const displayError=(error)=>{
+        displayArea.textContent = error.message
+    }
+
+    //UserMedia
+    const startStream = document.querySelector("#startStream").addEventListener("click",async()=>{
+      const stream = await navigator.mediaDevices.getUserMedia({video:true})
+        
+      const videoElement = document.querySelector("#videoElement");
+      videoElement.srcObject = stream;
+    })
