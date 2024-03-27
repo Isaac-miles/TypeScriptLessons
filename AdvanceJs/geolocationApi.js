c58abcbbf24c4017768f28830be335e34e185a94
isaac-miles <isaacnice247@gmail.com>
2024-03-27 11:38:21 +0100

    .

--- AdvanceJs/geolocationApi.js
+++ AdvanceJs/geolocationApi.js
@@ -1,2 +1,9 @@
    //Geolocation API: allows users to share their location with web applications
+
+   const {log} = console;
+
+    if(navigator.geolocation){
+        log("access",navigator)
+    }
     
+    const navigateBtn = document.querySelector("")
