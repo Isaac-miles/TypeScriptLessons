01dbb7dc41e868eab6f8f2d53f78acbe100ac36b
isaac-miles <isaacnice247@gmail.com>
2024-03-28 21:54:11 +0100

    .

--- AdvanceJs/webAPIs.js
+++ AdvanceJs/webAPIs.js
@@ -49,12 +49,21 @@
 
     let oscillator = null;
     playBtn.addEventListener("click",()=>{
+        if(oscillator) return;
          oscillator = context.createOscillator();
          oscillator.type ="sawtooth";
          oscillator.frequency.value = slider.value;
          oscillator.connect(context.destination)
          oscillator.start();
+    });
+
+    slider.addEventListener("input",e =>{
+        const frequency = e.target.value;
+        if(oscillator){
+            oscillator.frequency.value = frequency;
+        }
     })
+
     stopBtn.addEventListener("click",()=>{
         if(oscillator){
             oscillator.stop()
