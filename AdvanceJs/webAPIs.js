0e56d97db5cf7b576deae2581ee9fb654aa6e6b7
isaac-miles <isaacnice247@gmail.com>
2024-03-28 21:49:34 +0100

    .

---
+++ AdvanceJs/webAPIs.js
@@ -0,0 +1,63 @@
+//Performance ApI
+    //Allows measurement of the performance of web pages and web apps, it basically gives us access to very high precision timing data, and we can find the timing data on how long resources take to load, or how long function takes to run.
+    
+    function bubbleSort(arr) {
+        let len = arr.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    
+    const largeArray = Array.from({length:10000},()=>{
+        return Math.floor(Math.random()*1000);
+    })
+
+    const arrayForBubbleSort = [...largeArray];
+    const arrayForNativeSort = [...largeArray]
+
+    performance.mark("nativeSortStart")
+    arrayForNativeSort.sort((a,b)=>a-b);
+    performance.mark("nativeSortEnds")
+
+    performance.measure("Native sort Time","nativeSortStart","nativeSortEnds")
+
+    performance.mark("bubbleSortStart")
+    bubbleSort(arrayForBubbleSort);
+    performance.mark("bubbleSortStop")
+
+    performance.measure("Bubble sort Time","bubbleSortStart","bubbleSortStop")
+
+    const nativeDuration =performance.getEntriesByName("Native sort Time")[0].duration;
+
+    const bubbleDuration =performance.getEntriesByName("Bubble sort Time")[0].duration;
+    
+    // log("Native sort took ",nativeDuration)
+    // log("bubble sort took ",bubbleDuration)
+
+    //web Audio APi:Allows for the processing and synthesizing of audio in web applications.
+    const context = new AudioContext();
+    const slider = document.querySelector("#freqSlider");
+    const playBtn = document.querySelector("#playBtn");
+    const stopBtn = document.querySelector("#stopBtn");
+
+    let oscillator = null;
+    playBtn.addEventListener("click",()=>{
+         oscillator = context.createOscillator();
+         oscillator.type ="sawtooth";
+         oscillator.frequency.value = slider.value;
+         oscillator.connect(context.destination)
+         oscillator.start();
+    })
+    stopBtn.addEventListener("click",()=>{
+        if(oscillator){
+            oscillator.stop()
+            oscillator = null;
+        }
+    })
