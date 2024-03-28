e725b05eabd68c9bf5702a851336ec9e68aec59c
isaac-miles <isaacnice247@gmail.com>
2024-03-28 19:45:02 +0100

    performance

--- AdvanceJs/geolocationApi.js
+++ AdvanceJs/geolocationApi.js
@@ -100,8 +100,34 @@
         const ads = document.querySelectorAll(".ad");
         ads.forEach((ad)=>observer.observe(ad))
 
-        //Performance ApI
-        //Allows measurement of the performance of web pages and web apps, it basically gives us access to very high precision timing data, and we can find the timing data on how long resources take to load, or how long function takes to run.
-        const largeArray = Array.from({length:1000},()=>{
-            return Math.floor(Math.random()*1000);
-        })
+    //Performance ApI
+    //Allows measurement of the performance of web pages and web apps, it basically gives us access to very high precision timing data, and we can find the timing data on how long resources take to load, or how long function takes to run.
+    
+    function bubbleSort(arr) {
+        let len = arr.length;
+        for (let i = 0; i < len; i++) {
+            for (let j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    
+    const largeArray = Array.from({length:10000},()=>{
+        return Math.floor(Math.random()*1000);
+    })
+
+    const arrayForBubbleSort = [...largeArray];
+    const arrayForNativeSort = [...largeArray]
+
+    performance.mark("nativeSortStart")
+    arrayForNativeSort.sort((a,b)=>a-b);
+    performance.mark("nativeSortEnds")
+
+    performance.mark("bubbleSortStart")
+    bubbleSort(arrayForBubbleSort);
+    
