1859114927419cc59c42c069f9a02281149c8067
isaac-miles <isaacnice247@gmail.com>
2024-03-28 21:41:28 +0100

    .

--- AdvanceJs/geolocationApi.js
+++ AdvanceJs/geolocationApi.js
@@ -145,3 +145,6 @@
 
     //web Audio APi:Allows for the processing and synthesizing of audio in web applications.
     const context = new AudioContext();
+    const slider = document.querySelector("#freqSlider");
+    const playBtn = document.querySelector("#playBtn")
+    const stopBtn = document.querySelector("#stopBtn")
