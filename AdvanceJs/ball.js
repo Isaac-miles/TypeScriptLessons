4de3810f34d122969bb70574b1c8102e085fecec
isaac-miles <isaacnice247@gmail.com>
2024-03-29 15:48:41 +0100

    .

--- AdvanceJs/ball.js
+++ AdvanceJs/ball.js
@@ -5,6 +5,16 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d")
 
+class Ball{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 canvas.addEventListener("click",(e)=>{
     log("clicked", e.clientX,e.clientY)
+    ctx.beginPath();
+    ctx.arc( e.clientX,e.clientY,20,0,Math.PI*2);
+    ctx.fill()
 })
