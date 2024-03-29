cefd783d12dd1a50f7ef23128f3327b7b15e15ed
isaac-miles <isaacnice247@gmail.com>
2024-03-29 15:42:32 +0100

    .

--- AdvanceJs/ball.js
+++ AdvanceJs/ball.js
@@ -1,5 +1,10 @@
 const {log} = console;
 
 const canvas = document.querySelector("#ballCanvas");
-canvas.clientWidth = window.innerWidth;
-canvas.clientHeight = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d")
+
+canvas.addEventListener("click",()=>{
+    
+})
